e1a5e1717f234940053f534877886cc9789b965d
ayushd.nisg <techexecutive24-yp23@uidai.net.in>
2025-12-22 15:47:30 +0530

    staging config

--- main.go
+++ main.go
@@ -23,8 +23,8 @@ func main() {
 	routes.SetupRoutes(router)
 
 	// Start server
-	fmt.Println("Server starting on http://10.10.79.27:31151")
-	router.Run("10.10.79.27:31151")
+	fmt.Println("Server starting on http://0.0.0.0:31151")
+	router.Run("0.0.0.0:31151")
 }
 
 
